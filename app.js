db9e7d4bc6ddf77ab7cac00a97f34a2bfc09d17f
mLordswe <alexander.csore@gmail.com>
2024-09-06 12:58:45 +0200

    code

---
+++ app.js
@@ -0,0 +1,24 @@
+console.log("Flödesövningar");
+
+// let unPeeledPotatoes = 10;
+// for (let i = 0; i < 10 ; i++) {
+//     unPeeledPotatoes = unPeeledPotatoes -1;
+//     console.log(`Du har ${unPeeledPotatoes} potatisar kvar att skala`)
+    
+// }
+
+let marbles = 0
+for (let i = 0; i < 5; i++) {
+    marbles = marbles + 1
+    console.log(`Du har skopat upp ${marbles} glaskulor`)
+    
+}
+
+marbles = 0
+while(marbles < 5 ){
+    
+    marbles++
+    console.log(`du har skopat upp ${marbles} glasskulor`)
+    
+}
+
